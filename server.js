207b99b4a5d5c3faa99e1cd20075131c01443889
abdulla <abdulla.arish@protonmail.com>
2022-11-24 19:58:49 +0100

    production for render.com

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const express = require("express")
+const app = express()
+
+const dotenv = require("dotenv").config()
+const connectDB = require("./config/db.js")
+
+const colors = require("colors")
+
+const { errorHandler } = require("./middleware/errorMiddleware")
+
+
+connectDB()
+
+
+
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use("/api/lughet", require("./routes/lughetRoutes"))
+app.use("/api/editor", require("./routes/editorRoutes"))
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(`${__dirname}/build/index.html)`)
+  });
+}
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+
+  console.log(`Server is running on port: ${port}`);
+});
